131483062dc8595e12a5fb8589a932b1d0dd9bf9
AvhikBiswas <abhikbiswas351@gmail.com>
2024-02-04 04:31:33 +0530

    style: added  Create account btn

--- app/components/LoginComponents/Main_login.tsx
+++ app/components/LoginComponents/Main_login.tsx
@@ -8,8 +8,8 @@ export const Main_login = () => {
   return (
     <div className="grid grid-cols-12 h-screen w-screen">
       {/* logo  */}
-
-      <div className="col-span-6 border border-neutral-700 flex justify-center items-center pb-2 ">
+        
+      <div className="col-span-6 flex justify-center items-center pb-2 ">
         <div className="mb-7 ml-14">
           <RiTwitterXFill size={330} />
         </div>
@@ -17,7 +17,7 @@ export const Main_login = () => {
 
       {/* login */}
 
-      <div className="col-span-6  border border-neutral-700">
+      <div className="col-span-6 w-full">
         <div className="flex flex-col pl-20 pt-28 ">
           <span>
             <h1 className="font-bold text-7xl ">Happening now</h1>
@@ -25,12 +25,17 @@ export const Main_login = () => {
           <span className="pt-16 font-bold text-3xl">
             <h2>Join today.</h2>
           </span>
-          <div className="w-96">
+
+          <div className="w-96 rounded-full pt-6 m-1">
             <GoogleLogin
               onSuccess={cred=>console.log(cred)}
             />
-
           </div>
+
+            <div className="flex mt-6 ">
+                <button className="bg-[#1D9BF0] font-semibold justify-center items-center w-60 h-10 text-white rounded-3xl">Create account</button>
+            </div>
+
         </div>
       </div>
     </div>

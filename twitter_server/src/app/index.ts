9e36de78b1e542cc60fa088f59028e1ddc487881
AvhikBiswas <abhikbiswas351@gmail.com>
2024-02-06 00:46:09 +0530

    initial graphql setup

---
+++ twitter_server/src/app/index.ts
@@ -0,0 +1,31 @@
+import express from "express";
+import { ApolloServer } from "@apollo/server";
+import { expressMiddleware } from "@apollo/server/express4";
+import bodyParser from "body-parser";
+
+export async function initialServer() {
+  const app = express();
+
+  // Use express built-in middleware for parsing URL-encoded bodies
+  app.use(bodyParser .json());
+
+  const server = new ApolloServer({
+    typeDefs: `
+      type Query {
+        sayHello: String
+      }
+    `,
+    resolvers: {
+      Query: {
+        sayHello: () => "hello from graphql",
+      },
+    },
+  });
+
+  await server.start();
+
+  // Use expressMiddleware with the ApolloServer instance
+  app.use("/graphql", expressMiddleware(server));
+
+  return app;
+}
--- twitter_server/src/index.ts
+++ twitter_server/src/index.ts
@@ -1,1 +1,11 @@
-console.log("hi");
+import { initialServer } from "./app";
+
+async function init() {
+    const app=await initialServer();
+     app.listen(8000,()=>{
+        console.log('srver is running on 8000');
+    })
+    
+}
+
+init();

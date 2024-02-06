dca7732cd0aa1d95f3c0ad46245f11a10abdaef7
AvhikBiswas <abhikbiswas351@gmail.com>
2024-02-06 23:34:44 +0530

    git error

--- twitter_server/dist/user/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.user = void 0;
-const queries_1 = require("./queries");
-const resolvers_1 = require("./resolvers");
-const types_1 = require("./types");
-exports.user = { queries: queries_1.queries, resolvers: resolvers_1.resolvers, types: types_1.types };

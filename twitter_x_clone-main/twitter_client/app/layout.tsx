302b0566ce82e05ae1bd7e54240461c30293be9e
AvhikBiswas <abhikbiswas351@gmail.com>
2024-03-02 04:07:10 +0530

    rfac:added providers

--- twitter_x_clone-main/twitter_client/app/layout.tsx
+++ twitter_x_clone-main/twitter_client/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { ReactQuaryClientProvider } from "./components/ReactQuaryClientProvider";
+import { ReactQuaryClientProvider } from "../app/Providers/ReactQuaryClientProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 

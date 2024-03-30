e0998ebb23473d44c48218084da5b5b7f3920f6e
AvhikBiswas <abhikbiswas351@gmail.com>
2024-03-30 23:14:20 +0530

    refac: deply

--- twitter_x_clone-main/twitter_server/src/index.ts
+++ twitter_x_clone-main/twitter_server/src/index.ts
@@ -1,14 +1,12 @@
 import { initialServer } from "./app";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
-
 async function init() {
-    const app=await initialServer();
-     app.listen(8000,()=>{
-        console.log('srver is running on 8000');
-    })
-    
+  const app = await initialServer();
+  app.listen(process.env.PORT || 8000, () => {
+    console.log("srver is running on 8000");
+  });
 }
 
 init();

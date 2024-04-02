5ffe3fd11c81d1221430cd4e875ca78f3853479d
AvhikBiswas <abhikbiswas351@gmail.com>
2024-04-02 19:32:22 +0530

    fix added graphqlClient

--- twitter_x_clone-main/twitter_client/clients/api.ts
+++ twitter_x_clone-main/twitter_client/clients/api.ts
@@ -1,8 +1,8 @@
 import { GraphQLClient } from "graphql-request";
 
-
+export const graphqlClient = new GraphQLClient(process.env.API_ENDPOINT as string);
 export const graphqlClientHeder = new GraphQLClient(
-  process.env.NEXT_PUBLIC_SERVER_URL!,
+  process.env.NEXT_PUBLIC_SERVER_URL as string,
   {
     headers: () => ({
       Authorization: `${localStorage.getItem("_Autherization")}`,

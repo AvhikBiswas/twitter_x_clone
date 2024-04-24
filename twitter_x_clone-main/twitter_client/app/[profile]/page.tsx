1401f28ab912ac9000d86e8f5283485b647c0450
AvhikBiswas <abhikbiswas351@gmail.com>
2024-04-25 03:43:27 +0530

    fix: typo in profile

--- twitter_x_clone-main/twitter_client/app/[profile]/page.tsx
+++ twitter_x_clone-main/twitter_client/app/[profile]/page.tsx
@@ -1,4 +1,3 @@
-
 "use client";
 import React from "react";
 import ProfileLayout from "../layouts/ProfileLayout";
@@ -7,21 +6,22 @@ import { useCurrentUser } from "../hooks/useCurrentUser";
 import toast from "react-hot-toast";
 import { useRouter } from "next/navigation";
 
- const page=()=>{
-  const router=useRouter();
+const Page = () => {
+  const router = useRouter();
   const ProfileCard = dynamic(() => import("../components/ProfileCard"));
-  const { user,error } = useCurrentUser();
-  if(error){
-    localStorage.removeItem("_Autherization");
-    toast.error('Somthing Went wrong',{id:'10'});
-    router.push('/')
+  const { user, error } = useCurrentUser();
+  if (error) {
+    localStorage.removeItem("_Authorization");
+    toast.error("Something went wrong", { id: "10" });
+    router.push("/");
   }
   return (
-      <ProfileLayout>
-        <div className="flex w-full">
-          <ProfileCard />
-        </div>
-      </ProfileLayout>
+    <ProfileLayout>
+      <div className="flex w-full">
+        <ProfileCard />
+      </div>
+    </ProfileLayout>
   );
 }
-export default page;
+
+export default Page; // Change export name to start with uppercase letter
